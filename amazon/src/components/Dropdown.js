ed469a67e6105d43995948813dbcbb36057a249a
RaghavDixit <71072986+RaghavDixit@users.noreply.github.com>
2020-09-13 15:57:48 +0530

    Add files via upload

---
+++ amazon/src/components/Dropdown.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Dropdown = ()=>{
+    return <select>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+   </select>     
+};
+export default Dropdown;
